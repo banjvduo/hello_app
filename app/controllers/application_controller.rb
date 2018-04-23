b833846e5ee3b6c3b86a105108d66cfa7fe6b472
banjvduo <stagry@gmail.com>
2018-04-24 00:52:49 +0800

    add random string

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "¡hello,world!"
+    render html: "¡hello,world! "+('a'..'z').to_a.shuffle[0..7].join
   end
   
 end
